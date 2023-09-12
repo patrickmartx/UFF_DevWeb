42683650f58bf61a4e673ab6499a59426b5f804e
Patrick Martins <xavier.patrickmartins@gmail.com>
2023-09-12 19:49:44 -0300

    first commit

---
+++ projetoDevWeb1/src/java/Cont.java
@@ -0,0 +1,64 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author cbraga
+ */
+@WebServlet(urlPatterns = {"/Cont"})
+public class Cont extends HttpServlet {
+
+    int cont = 0 ;
+   
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        PrintWriter out = response.getWriter() ;
+        out.println("<!DOCTYPE HTML> <html><body>");
+        out.print(this.cont);
+        out.print("<form action=Cont method=\"post\">") ;
+        out.print("<input type=\"submit\" value=\"Cont\">") ;
+        out.print("</form>");
+        out.println("</body></html>");
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+       this.cont ++;
+       response.sendRedirect("Cont");
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
